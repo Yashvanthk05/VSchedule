babd7cc47131623d01d7fca9c7975371b261d5c1
Yashvanthk05 <yashvanthk05@gmail.com>
2025-10-26 11:24:53 +0530

    new logic

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,6 +1,5 @@
 import { slotList } from './slotList';
 import falldata from '../models/winter.json';
-import { PiCheckSquareOffsetDuotone } from 'react-icons/pi';
 
 export const getData = () => {
   if (localStorage.getItem('ffcs_planner_data'))
@@ -35,80 +34,82 @@ export const transform = (slots) => {
 };
 
 export const generate = () => {
-  if (localStorage.getItem('ffcs_planner_data')) {
-    const data = JSON.parse(localStorage.getItem('ffcs_planner_data'));
-    console.log(data);
-    let optimized = {};
-    data.forEach((subject) => {
-      const all = falldata.filter((item) => item['COURSE CODE'] === subject.code);
-      let uniqueSlots;
-      if (subject.slots) {
-        uniqueSlots = new Array(subject.slots);
-      } else {
-        uniqueSlots = Array.from(
-          new Set(
-            all.map((item) =>
-              item['SLOT']
-                .split('+')
-                .map((i) => `_${i.trim()}_`)
-                .join(',')
-            )
+  if (!localStorage.getItem('ffcs_planner_data')) return;
+  const data = JSON.parse(localStorage.getItem('ffcs_planner_data'));
+  const optimized = {};
+  data.forEach((subject) => {
+    const all = falldata.filter((item) => item['COURSE CODE'] === subject.code);
+    let uniqueSlots;
+    if (subject.slots) {
+      uniqueSlots = [subject.slots];
+    } else {
+      uniqueSlots = Array.from(
+        new Set(
+          all.map((item) =>
+            item['SLOT']
+              .split('+')
+              .map((i) => `_${i.trim()}_`)
+              .join(',')
           )
-        );
+        )
+      );
+    }
+    optimized[subject.code] = uniqueSlots;
+  });
+
+  const subjects = Object.keys(optimized);
+  const n = subjects.length;
+
+  const partIndex = new Map();
+  let nextPart = 0;
+
+  const subjectOptions = {};
+  for (const subject of subjects) {
+    const slotsArr = optimized[subject] || [];
+    const opts = [];
+    for (const slot of slotsArr) {
+      const slotParts = slot.split(',').map((p) => p.trim());
+      let mask = 0n;
+      for (const part of slotParts) {
+        if (!partIndex.has(part)) {
+          partIndex.set(part, nextPart++);
+        }
+        const idx = BigInt(partIndex.get(part));
+        mask |= 1n << idx;
       }
-      optimized[subject.code] = uniqueSlots;
-    });
-    const subjects = Object.keys(optimized);
-    const results = [];
-    const n = subjects.length;
-    function backtrack(index) {
-      if (index === n) {
-        results.push(Array.from(chosen));
-        return;
-      }
-      const subject = subjects[index];
-      const slotsArr = optimized[subject];
-      if (slotsArr.length === 1) {
-        const slot = slotsArr[0];
-        const slotParts = slot.split(',');
-        let clash = false;
-        for (const part of slotParts) {
-          if (used.has(part)) {
-            clash = true;
-            break;
-          }
-        }
-        if (!clash) {
-          slotParts.forEach((p) => used.add(p));
-          chosen[index] = `${subject}-${slot}`;
-          backtrack(index + 1);
-          slotParts.forEach((p) => used.delete(p));
-        }
-        return;
-      }
-      for (let i = 0; i < slotsArr.length; ++i) {
-        const slot = slotsArr[i];
-        const slotParts = slot.split(',');
-        let clash = false;
-        for (let j = 0; j < slotParts.length; ++j) {
-          if (used.has(slotParts[j])) {
-            clash = true;
-            break;
-          }
-        }
-        if (!clash) {
-          slotParts.forEach((p) => used.add(p));
-          chosen[index] = `${subject}-${slot}`;
-          backtrack(index + 1);
-          slotParts.forEach((p) => used.delete(p));
-        }
+      opts.push({ slot, parts: slotParts, mask });
+    }
+    subjectOptions[subject] = opts;
+  }
+
+  const orderedSubjects = subjects.slice().sort((a, b) => {
+    return (subjectOptions[a].length || 0) - (subjectOptions[b].length || 0);
+  });
+
+  const results = [];
+  const chosen = new Array(n);
+
+  function backtrack(index, usedMask) {
+    if (index === n) {
+      results.push(Array.from(chosen));
+      return;
+    }
+    const subject = orderedSubjects[index];
+    const opts = subjectOptions[subject] || [];
+    if (opts.length === 0) {
+      return;
+    }
+    for (let i = 0; i < opts.length; ++i) {
+      const opt = opts[i];
+      if ((usedMask & opt.mask) === 0n) {
+        chosen[index] = `${subject}-${opt.slot}`;
+        backtrack(index + 1, usedMask | opt.mask);
       }
     }
-    const chosen = new Array(n);
-    const used = new Set();
-    backtrack(0);
-    return results;
   }
+
+  backtrack(0, 0n);
+  return results;
 };
 
 export const generate_transform = (slots) => {
